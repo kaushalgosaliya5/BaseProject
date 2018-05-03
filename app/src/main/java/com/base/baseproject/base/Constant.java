92b2e62688362ee04a8e0e680cd1abde2fc5044c
kaushalgosaliya5 <kaushalgosaliya5@gmail.com>
2018-05-03 21:35:13 +0530

    facebook shimmer effect

--- app/src/main/java/com/base/baseproject/base/Constant.java
+++ app/src/main/java/com/base/baseproject/base/Constant.java
@@ -2,4 +2,7 @@ package com.base.baseproject.base;
 
 public class Constant {
 
+    public static final String PREF_USER_NAME = "username";
+    public static final String FONT_HELVETICA_CONDENSED_BOLD = "font/helvetica_condensed_bold.ttf";
+
 }
